bb6d792f171bd40721e5db91df06c99eb01eba2b
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-02 16:37:35 +0900

    feat: prepare for questionniares

--- app/routes/questionnaires.begin.tsx
+++ app/routes/questionnaires.begin.tsx
@@ -5,16 +5,20 @@ import LoadingSpinner from "~/components/LoadingSpinner";
 export default function BeginQuestionnaire() {
   const navigation = useNavigation();
   return (
-    <Link to="/game">
-      <button
-        className={`mt-4 rounded-md border-2 border-black px-4 py-2 text-center text-xl transition-all ${navigation.state === "loading" ? "cursor-not-allowed" : "hover:bg-black hover:text-white"}`}
-        disabled={navigation.state === "loading"}
-      >
-        {navigation.state === "loading" && (
-          <LoadingSpinner size="sm" position="inline" color="primary" />
-        )}
-        {navigation.state === "loading" ? "Loading..." : "Start"}
-      </button>
-    </Link>
+    <div className="mx-auto my-12 flex flex-col items-center justify-center text-slate-950 dark:text-slate-100">
+      <h1 className="text-4xl text-red-500">Questionnaire goes here</h1>
+      {/* TODO: Change Link to a form */}
+      <Link to="/game">
+        <button
+          className={`mt-4 rounded border-2 border-indigo-500 px-4 py-2 text-center text-2xl font-bold text-indigo-500 transition-all ${navigation.state === "loading" ? "cursor-not-allowed" : "hover:border-indigo-700 hover:bg-indigo-700 hover:text-slate-50"}`}
+          disabled={navigation.state === "loading"}
+        >
+          {navigation.state === "loading" && (
+            <LoadingSpinner size="sm" position="inline" color="primary" />
+          )}
+          {navigation.state === "loading" ? "Saving..." : "Submit"}
+        </button>
+      </Link>
+    </div>
   );
 }
--- app/routes/questionnaires.end.tsx
+++ app/routes/questionnaires.end.tsx
@@ -1,3 +1,22 @@
+import LoadingSpinner from "~/components/LoadingSpinner";
+import { useNavigation } from "@remix-run/react";
+
 export default function EndQuestionnaire() {
-  return <div>This is the end of the game</div>;
+  const navigation = useNavigation();
+
+  return (
+    <div className="mx-auto my-12 flex flex-col items-center justify-center text-slate-950 dark:text-slate-100">
+      <h1 className="text-4xl text-red-500">Questionnaire goes here</h1>
+      {/* TODO: Change Link to a form */}
+      <button
+        className={`mt-4 rounded border-2 border-indigo-500 px-4 py-2 text-center text-2xl font-bold text-indigo-500 transition-all ${navigation.state === "loading" ? "cursor-not-allowed" : "hover:border-indigo-700 hover:bg-indigo-700 hover:text-slate-50"}`}
+        disabled={navigation.state === "loading"}
+      >
+        {navigation.state === "loading" && (
+          <LoadingSpinner size="sm" position="inline" color="primary" />
+        )}
+        {navigation.state === "loading" ? "Saving..." : "Submit"}
+      </button>
+    </div>
+  );
 }
