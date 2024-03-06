87be58be41d3be94d41fbcaa29e418b1d8ee22ae
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-06 17:14:23 +0900

    feat: add finish page

---
+++ app/routes/finish.tsx
@@ -0,0 +1,10 @@
+export default function Finish() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-slate-950 dark:text-slate-50">
+      <p className="mx-auto px-36 text-center text-4xl font-bold ">
+        🎉 Thank you for participating in our research! 🎉
+      </p>
+      <p className="mt-4 text-3xl">Please feel free to close this tab.</p>
+    </div>
+  );
+}
