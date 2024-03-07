3fea38a8e475e0a04e08fe060bb90a73d6a82f6c
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-03-07 13:36:35 +0900

    chore: formatted

--- app/components/ui/forms/ChoiceQuestion.tsx
+++ app/components/ui/forms/ChoiceQuestion.tsx
@@ -33,7 +33,7 @@ export default function ChoiceQuestion({
             className="group my-2 flex cursor-pointer rounded-lg border px-4 transition-colors hover:bg-indigo-50 has-[:checked]:border-indigo-600 has-[:checked]:bg-indigo-100 dark:hover:bg-zinc-700 dark:has-[:checked]:border-indigo-700 dark:has-[:checked]:bg-inherit"
             key={option.value}
           >
-            <div className="h-4 w-4 self-center rounded-full border-4 border-slate-50 bg-slate-50 group-has-[:checked]:border-2 group-has-[:checked]:bg-indigo-600 transition-all" />
+            <div className="h-4 w-4 self-center rounded-full border-4 border-slate-50 bg-slate-50 transition-all group-has-[:checked]:border-2 group-has-[:checked]:bg-indigo-600" />
             <input
               className="h-0 w-0 cursor-pointer opacity-0"
               type="radio"
