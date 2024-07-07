d6331a45929f0ceca963031e9984bfb52c3fca3f
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-07-07 19:13:10 +0900

    feat: update story

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { ActionFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { type MetaFunction } from "@remix-run/node";
 import { Form, redirect, useNavigation } from "@remix-run/react";
 import LoadingSpinner from "~/components/LoadingSpinner";
 import { getFirstStoryChunkId } from "~/db/stories";
@@ -10,13 +10,8 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function action({ request }: ActionFunctionArgs) {
-  const approach = (await request.formData()).get("approach");
-
-  const storyId =
-    approach === "baseline"
-      ? "d979223f-f4e2-11ee-b819-182649966cd4"
-      : "fff66562-f284-11ee-b947-00155d2625d3";
+export async function action() {
+  const storyId = "eb8b2688-3c3e-11ef-a407-9a01b5b45ca4";
   const firstChunkId = await getFirstStoryChunkId(storyId);
 
   return redirect(`/game/${storyId}/${firstChunkId}`);
@@ -29,23 +24,10 @@ export default function Index() {
     <div className="mx-auto h-screen w-screen flex-col px-16 text-slate-950 lg:w-4/5 lg:px-8 dark:text-slate-100">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <h1 className="mb-8 text-center text-3xl font-bold md:text-4xl">
-          🌌 Multiverse of Greatness 🌠
+          🌌 A Tale Retold 🌠
         </h1>
         <div className="flex flex-col gap-8 md:flex-row">
           <Form action="?index" method="POST" className="self-center">
-            <input type="hidden" name="approach" value="baseline" />
-            <button
-              className="rounded border-2 border-indigo-500 px-4 py-2 text-center text-2xl font-bold text-indigo-500 transition-all hover:border-indigo-700 hover:bg-indigo-700 hover:text-slate-50"
-              disabled={navigation.state === "loading"}
-            >
-              {navigation.state === "loading" && (
-                <LoadingSpinner size="sm" position="inline" color="primary" />
-              )}{" "}
-              Best Baseline Story
-            </button>
-          </Form>
-          <Form action="?index" method="POST" className="self-center">
-            <input type="hidden" name="approach" value="proposed" />
             <button
               className="rounded border-2 bg-indigo-600 px-4 py-2 text-center text-2xl font-bold text-zinc-100 transition-all hover:border-indigo-700 hover:bg-indigo-700 hover:text-slate-50"
               disabled={navigation.state === "loading"}
@@ -53,7 +35,7 @@ export default function Index() {
               {navigation.state === "loading" && (
                 <LoadingSpinner size="sm" position="inline" color="primary" />
               )}{" "}
-              Best Proposed Story
+              Start Game
             </button>
           </Form>
         </div>
